0188e631b655670fc40f7a31681afeaeb3298165
etvx86 <64978924+EnterTheVoid-x86@users.noreply.github.com>
2022-06-14 20:44:53 -0400

    Add files via upload

--- debug.py
+++ debug.py
@@ -5,9 +5,12 @@ print("2. Exit")
 inp = input(">> ")
 if inp == "1":
   print("KERNEL PANIC: Force crash!")
-  os.system("killall python3")
-  os.system("killall python")
-  os.system("killall py")
+  print("Pyros is shutting down...")
+  if platform.system() == "unix":
+    os.system("kill -9 python3 > /dev/null")
+    os.system("kill -9 py > /dev/null")
+  else:
+    os.system("taskkill /f /pid py.exe > NUL")
 elif inp == "2":
   exit()
   
